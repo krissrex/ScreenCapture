bde1bbc170a9b5d27e4cbdceb565245cd16efacc
Kristian Rekstad <krirek@msn.com>
2014-07-04 02:39:22 +0200

    Removed a comment.

--- ScreenCapture/src/com/polarbirds/screenCapture/Main.java
+++ ScreenCapture/src/com/polarbirds/screenCapture/Main.java
@@ -4,9 +4,8 @@ package com.polarbirds.screenCapture;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Code
 		System.out.println("Started.");
-			CaptureFrame.run();
+		CaptureFrame.run();
 	}
 
 }
