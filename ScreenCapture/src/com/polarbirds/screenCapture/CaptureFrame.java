0f270805843282c452eec55da595e0c2618a8b44
Kristian Rekstad <krirek@msn.com>
2014-07-04 02:43:14 +0200

    The bounds sent for capturing are now screen coordinates instead of local coordinates. The overlay now hides before capturing. A variable thread sleep was added because the monitor was not updated before the capture. Needed due to full screen captures when no selection was made. Now works on Ubuntu/Unity becuase of frame.setVisible(false).

--- ScreenCapture/src/com/polarbirds/screenCapture/CaptureFrame.java
+++ ScreenCapture/src/com/polarbirds/screenCapture/CaptureFrame.java
@@ -5,26 +5,42 @@ import java.awt.AWTException;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.EventQueue;
+import java.awt.GraphicsConfiguration;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
 import java.awt.Rectangle;
 import java.awt.Toolkit;
+import java.awt.event.ComponentEvent;
+import java.awt.event.ComponentListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 import javax.swing.JFrame;
 
 public class CaptureFrame {
-	private JFrame frame;
-	private DragPanel dPanel;
+	private JFrame frame; //The parent frame
+	private DragPanel dPanel; //The displayed overlay, also containing the red TextArea
 	private MouseHandler mouse;
-	private Rectangle bound;
-	private Color TRANSPARENT_COLOR = MyColors.TRANSPARENT_COLOR;
-	public static int WIDTH;
-	public static int HEIGHT;
+	private Rectangle bound; //The user selected bound
+	private Color TRANSPARENT_COLOR = MyColors.TRANSPARENT_COLOR; //RGBA with alpha 0
+	private Rectangle frameBounds = new Rectangle();
+	private int minRefreshRate = Integer.MAX_VALUE;
 	
 	public CaptureFrame() {
-		Dimension d = Toolkit.getDefaultToolkit().getScreenSize();
-		WIDTH = d.width;
-		HEIGHT = d.height;
+		
+		//Time to get some multi monitor action going on!
+		GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+		GraphicsDevice[] gd = ge.getScreenDevices();
+		
+		for (GraphicsDevice monitor : gd) {
+			minRefreshRate = Math.min(minRefreshRate, monitor.getDisplayMode().getRefreshRate());
+			
+			for (GraphicsConfiguration config : monitor.getConfigurations()) {
+				frameBounds = frameBounds.union(config.getBounds()); //Union the bounds to get one Rectangle that spans across all monitors.
+			}
+		}
+		System.out.println(frameBounds); //FIXME debug.
+		
 		init();
 	}
 	
@@ -35,7 +51,6 @@ public class CaptureFrame {
 				try {
 					CaptureFrame window = new CaptureFrame();
 					window.frame.setVisible(true);
-					//window.dPanel.repaint();
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -52,10 +67,10 @@ public class CaptureFrame {
 		
 		frame.setUndecorated(true);
 		frame.setBackground(TRANSPARENT_COLOR);
-		frame.setBounds(0, 0, WIDTH, HEIGHT);
+		frame.setBounds(frameBounds);
 		frame.setLayout(null);
 		frame.add(dPanel);
-		dPanel.setBounds(0, 0, WIDTH, HEIGHT);
+		dPanel.setBounds(0, 0, frameBounds.width, frameBounds.height); //FIXME 0,0 may be incorrect. Use frameBounds instead
 		
 		
 		frame.addKeyListener(
@@ -81,12 +96,26 @@ public class CaptureFrame {
 		frame.addMouseMotionListener(mouse);
 		
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
-		dPanel.repaint();
-		
 	}
 	
 	private void capture() {
+		//Some black magic makes the screen capture actually work on Unity after this call to setVisible.
+		//I'm not here to make a screen capture of the screen capture program. I ain't that meta.
+		//Ironically the window does not get hidden before the screen is captured tho. Therefore the sleep.
+		frame.setVisible(false);
+		frame.dispose();
+		
+		try {
+			//This whole thing is a bad idea. But it works, sadly.
+			//I have to sleep for 10x refresh time since it seems to clear the frame by then.
+			int duration = Math.round(10000/minRefreshRate);
+			System.out.println(duration);
+			
+			Thread.currentThread().sleep(duration);
+		} catch (InterruptedException e1) {
+			e1.printStackTrace();
+		}
+		
 		ScreenCapturer capt;
 		
 		try {
@@ -95,13 +124,16 @@ public class CaptureFrame {
 			System.err.println("Error in creating robot. "+e);
 			e.printStackTrace();
 			frame.dispose();
-			return;
+			return; //It all went to hell, so I'm giving up on you, lad!
 		}
 		
-		capt.capture(bound);
+		if (mouse.getScreenRelativeBounds().width == 0 || mouse.getScreenRelativeBounds().height == 0) {
+			capt.capture(frameBounds);
+		} else {
+			capt.capture(mouse.getScreenRelativeBounds());
+		}
 		FileSaver.setImage(capt.getImage());
 		FileSaver.run();
-		frame.dispose();
 	}
 	
 	public void draw(Rectangle bound) {
