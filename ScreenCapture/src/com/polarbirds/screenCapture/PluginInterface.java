4cefcdc5ee312afc4b62a8635155c33e5183dc7c
trondhumbor <trondhumbor@gmail.com>
2015-02-27 00:35:18 +0100

    Added a manifest file, for better plugin-description.

--- ScreenCapture/src/com/polarbirds/screenCapture/PluginInterface.java
+++ ScreenCapture/src/com/polarbirds/screenCapture/PluginInterface.java
@@ -3,5 +3,6 @@ package com.polarbirds.screenCapture;
 import java.awt.image.BufferedImage;
 
 public interface PluginInterface {
+    public Manifest Manifest();
     public void run(BufferedImage img);
 }
