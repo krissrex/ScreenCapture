315950942df00989a20c3d3d24a14b285925a8a2
Kristian Rekstad <krirek@msn.com>
2015-03-03 00:58:43 +0100

    Update PluginHandler.java
    Print plugin name instead of string array, to get useful print statements.
    Changed plural of 'plugin' to also occur on size 0.

--- ScreenCapture/src/com/polarbirds/screencapture/plugin/PluginHandler.java
+++ ScreenCapture/src/com/polarbirds/screencapture/plugin/PluginHandler.java
@@ -30,23 +30,23 @@ public class PluginHandler {
 
                 System.out.println(newPlugin.manifest()); //Debug
             } catch (ClassCastException | InstantiationException | IllegalAccessException ex){
-                System.err.println("Failed to load \""+plugin+"\" as a plugin.");
+                System.err.println("Failed to load \""+plugin[1]+"\" as a plugin.");
                 ex.printStackTrace();
             } catch (ClassNotFoundException | MalformedURLException ex){
-                System.err.println("Could not find plugin \""+plugin+"\".");
+                System.err.println("Could not find plugin \""+plugin[1]+"\". Please verify url.");
                 ex.printStackTrace();
             } catch (Exception ex){
                 /* Plugins may fail to initialize properly, which would
                  * leave them in an invalid state. We should therefore let them throw exceptions
                  * which will make it possible for us to ignore the plugin that failed.
                  */
-                System.err.println("Other exception occured while loading \""+plugin+"\"" );
+                System.err.println("Unknown exception occured while loading \""+plugin[1]+"\"" );
                 ex.printStackTrace();
             }
 
         }
 
-        System.out.println("Loaded "+loadedPlugins.size()+" plugin"+(loadedPlugins.size()>1?"s":"")+".");
+        System.out.println("Loaded "+loadedPlugins.size()+" plugin"+(loadedPlugins.size()==1?"":"s")+".");
     }
 
     public List<PluginInterface> getLoadedPlugins(){
