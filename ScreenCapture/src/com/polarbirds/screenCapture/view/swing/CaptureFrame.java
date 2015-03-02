904baa05ae41883bc84b80ff49a2969658aa2b37
Kristian Rekstad <krirek@msn.com>
2015-03-02 14:45:59 +0100

    Fixed linux bug Fixed bug where init opened window.

--- ScreenCapture/src/com/polarbirds/screenCapture/view/swing/CaptureFrame.java
+++ ScreenCapture/src/com/polarbirds/screenCapture/view/swing/CaptureFrame.java
@@ -20,7 +20,8 @@ public class CaptureFrame implements View{
 	private MouseHandler mouseHandler;
 	private static final Color TRANSPARENT_COLOR = MyColors.TRANSPARENT_COLOR; //RGBA with alpha 0
 	private Rectangle frameBounds = new Rectangle();
-	private int minRefreshRate = Integer.MAX_VALUE;
+
+    private boolean needsSleep = (System.getProperty("os.name").toLowerCase()).startsWith("linux");
 
     private Controller.OnCaptureListener listener;
 
@@ -33,80 +34,67 @@ public class CaptureFrame implements View{
 		GraphicsDevice[] gd = ge.getScreenDevices();
 		
 		for (GraphicsDevice monitor : gd) {
-			minRefreshRate = Math.min(minRefreshRate, monitor.getDisplayMode().getRefreshRate());
-			// OSX could make monitor.getDisplayMode().getRefreshRate() return 0, which
-            // would lead to a division by 0 further down the road.
-            minRefreshRate = Math.max(1, minRefreshRate);
-
 			for (GraphicsConfiguration config : monitor.getConfigurations()) {
 				frameBounds = frameBounds.union(config.getBounds()); //Union the bounds to get one Rectangle that spans across all monitors.
 			}
 		}
 	}
-	
-	
-	public void init() {
-		frame = new JFrame(TITLE);
-		dragPanel = new DragPanel();
-		mouseHandler = new MouseHandler(this);
-		
-		frame.setUndecorated(true);
-		frame.setBackground(TRANSPARENT_COLOR);
-		frame.setBounds(frameBounds);
-		frame.setLayout(null);
+
+
+    @Override
+    public void init(){
+        frame = new JFrame(TITLE);
+        dragPanel = new DragPanel();
+        mouseHandler = new MouseHandler(this);
+
+        frame.setUndecorated(true);
+        frame.setBackground(TRANSPARENT_COLOR);
+        frame.setBounds(frameBounds);
+        frame.setLayout(null);
 
 
         //frame.setOpacity(0.4f); //The colors are being drawn transparent already.
 
         dragPanel.setBounds(0, 0, frameBounds.width, frameBounds.height); //FIXME 0,0 may be incorrect. Use frameBounds.x .y instead
-		dragPanel.setDoubleBuffered(true);
+        dragPanel.setDoubleBuffered(true);
         dragPanel.setOpaque(false);
         frame.add(dragPanel);
 
-		frame.addKeyListener(
+        frame.addKeyListener(
                 new KeyListener() {
 
-					@Override
-					public void keyTyped(KeyEvent e) {}
-		
-					@Override
-					public void keyPressed(KeyEvent e) {}
-		
-					@Override
-					public void keyReleased(KeyEvent e) {
-						if (e.getKeyCode() == KeyEvent.VK_ESCAPE)
-							frame.dispose();
-						if (e.getKeyCode() == KeyEvent.VK_ENTER)
-							capture();
-					}
-					
-				});
-		
-		frame.addMouseListener(mouseHandler);
-		frame.addMouseMotionListener(mouseHandler);
-		
-		frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+                    @Override
+                    public void keyTyped(KeyEvent e) {}
 
-        frame.setVisible(true);
-	}
+                    @Override
+                    public void keyPressed(KeyEvent e) {}
 
+                    @Override
+                    public void keyReleased(KeyEvent e) {
+                        if (e.getKeyCode() == KeyEvent.VK_ESCAPE)
+                            frame.dispose();
+                        if (e.getKeyCode() == KeyEvent.VK_ENTER)
+                            capture();
+                    }
+
+                });
+
+        frame.addMouseListener(mouseHandler);
+        frame.addMouseMotionListener(mouseHandler);
+
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
 
 	private void capture() {
 		//Some black magic makes the screen capture actually work on Ubuntu's Unity after this call to setVisible.
-		//I'm not here to make a screen capture of the screen capture program. I ain't that meta.
-		//Ironically the window does not get hidden before the screen is captured tho. Therefore the sleep.
 		frame.setVisible(false);
 		frame.dispose();
 
-
-		
 		try {
-			//This whole thing is a bad idea. But it works, sadly.
-			//I have to sleep for 10x refresh time since it seems to clear the frame by then.
-			int duration = Math.round(10000/minRefreshRate);
-			System.out.println("Timeout duration before screen capture: "+duration);
-
-            Thread.sleep(duration);
+			//Ubuntu has window animations with a default 120ms transition time.
+            if (needsSleep) {
+                Thread.sleep(120);
+            }
 		} catch (InterruptedException e1) {
 			e1.printStackTrace();
 		}
@@ -133,7 +121,7 @@ public class CaptureFrame implements View{
         EventQueue.invokeLater(new Runnable() {
             public void run() {
                 try {
-                    CaptureFrame.this.init();
+                    CaptureFrame.this.frame.setVisible(true);
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
